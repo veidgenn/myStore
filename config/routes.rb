fc0071e0f3335f066f3e0987053cb967de5d6cda
Varvara <veidgenn@mail.ru>
2019-03-13 12:01:48 +0300

    Created store-controller. Added product catalog with CSS.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'store/index'
+
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'store#index', as: 'store'
 end
