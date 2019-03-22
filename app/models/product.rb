8148bae98c7237e19e5d122436b4c1a0b0c1f97d
Varvara <veidgenn@mail.ru>
2019-03-22 15:25:06 +0300

    Simple shopping cart created.

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,16 @@ class Product < ApplicationRecord
 		message: 'URL должен указывать на изображение формата GIF, JPG или PNG.' 
 	}
 
+	 has_many :line_items 
+	 before_destroy :ensure_not_referenced_by_any_line_item
+	  private
+	   # убеждаемся в отсутствии товарных позиций, ссылающихся на данный товар 
+	   def ensure_not_referenced_by_any_line_item 
+	   	if line_items.empty? 
+	   		return true 
+	   		else 
+	   			errors.add(:base, 'существуют товарные позиции') 
+	   			return false 
+	   		end 
+	   	end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190225185903) do
+ActiveRecord::Schema.define(version: 20190322114528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.bigint "product_id"
+    t.bigint "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -24,4 +38,6 @@ ActiveRecord::Schema.define(version: 20190225185903) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "products"
 end
